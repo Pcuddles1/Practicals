2ffb6ed81c27b4ef9688b82fecf148b684691e3f
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-04-21 14:06:39 +1000

    set initial parameters

---
+++ Prac_06/programming_language.py
@@ -0,0 +1,9 @@
+
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=True, year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
