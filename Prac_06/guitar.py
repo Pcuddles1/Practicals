8f721651a88ba43c035c77b2cbb420361e75dcb7
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-04-21 14:33:44 +1000

    fixed get_age() and print it with examples

--- Prac_06/guitar.py
+++ Prac_06/guitar.py
@@ -1,15 +1,15 @@
 
 
 class Guitar:
-    current_year = 2020
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
+        self.age = 0
 
     def __str__(self):
         return "{} ({}) : ${}".format(self.name, self.year, self.cost)
 
     def get_age(self):
-        age = current_year - year
+        age = 2020 - self.year
         return age
---
+++ Prac_06/guitar_test.py
@@ -0,0 +1,9 @@
+
+from Prac_06.guitar import Guitar
+
+guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar1 = Guitar("ye", 2001, 20)
+
+
+print(guitar.get_age())
+
