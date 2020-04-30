38ef42b31c88d85bfa7ff6e5fd7e3712fc559efe
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-04-30 14:58:30 +1000

    added yellow colour to label and now desplays input when button pushed

--- Prac_07/KivyDemos-master/convert_miles_km.py
+++ Prac_07/KivyDemos-master/convert_miles_km.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import StringProperty
 
 
 class ConvertDistanceApp(App):
+    message = StringProperty()
     def build(self):
         Window.size = (500, 200)
         self.title = "Converting Distances"
         self.root = Builder.load_file('convert_miles_km')
         return self.root
+    def convert_miles(self):
+        self.message = self.root.ids.user_input.text
 
 ConvertDistanceApp().run()
