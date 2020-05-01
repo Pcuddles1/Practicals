fdd3ddd183dd39eaa181347cddc9ae2322df4312
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-01 13:19:16 +1000

    initial commit

---
+++ Prac_07/KivyDemos-master/dynamic_labels.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+class DynamicLabelsApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        """Construct main app."""
+        self.names = ["Pat", "John", "Fred"]
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+
+    def create_widgets(self):
+
+        """Create buttons from dictionary entries and add them to the GUI."""
+        for name in self.names:
+            # create a button for each data entry, specifying the text and id
+            # (although text and id are the same in this case, you should see how this works)
+            temp_label = Button(text=name, id=name)
+            # add the button to the "entries_box" layout widget
+            self.root.ids.entries_box.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
