155c1fd0604b57714922461b742ea4b951febf44
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-08 17:47:04 +1000

    Initial commit

---
+++ Prac_08/silver_service_taxi.py
@@ -0,0 +1,5 @@
+from Prac_08.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    def __init__(self, name, fuel):
+        super().__init__(name, fuel)
---
+++ Prac_08/silver_service_taxi_test.py
@@ -0,0 +1,11 @@
+from Prac_08.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    silver_taxi = SilverServiceTaxi(100, "Silver")
+    print(silver_taxi)
+
+
+
+
+
+main()
