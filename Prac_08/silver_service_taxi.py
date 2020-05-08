e1d035c8ed7698b5e9ee07cae85455b1c5d9a9fd
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-08 17:56:07 +1000

    added fancy multiplier and flag cost

--- Prac_08/silver_service_taxi.py
+++ Prac_08/silver_service_taxi.py
@@ -1,5 +1,11 @@
 from Prac_08.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
-    def __init__(self, name, fuel):
+    flagfall = 4.5
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
+        self.fanciness = fanciness
+    def get_fare(self):
+        fare = super().get_fare()
+        fare = (fare*self.fanciness)+self.flagfall
+        return fare
--- Prac_08/silver_service_taxi_test.py
+++ Prac_08/silver_service_taxi_test.py
@@ -1,9 +1,10 @@
 from Prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
-    silver_taxi = SilverServiceTaxi(100, "Silver")
+    silver_taxi = SilverServiceTaxi(100, "Silver",2)
     print(silver_taxi)
-
+    silver_taxi.drive(20)
+    print(silver_taxi)
 
 
 
