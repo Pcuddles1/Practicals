c83c985d1fdf02e968464e5e1b0fa940a3c30955
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-08 14:47:19 +1000

    Initial commit

---
+++ Prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+from Prac_08.taxi import Taxi
+
+def main():
+    Prius = Taxi(100,"prius 1", 1.23)
+    print(Prius.fuel)
+    Prius.drive(40)
+    print("Name: {}\nOdometer: {}\nFuel: {}\nDistance: {}\nFare: {}".format(Prius.name, Prius.odometer, Prius.fuel, Prius.current_fare_distance,Prius.get_fare()))
+    Prius.start_fare()
+    Prius.drive(100)
+    print("\nName: {}\nOdometer: {}\nFuel: {}\nDistance: {}\nFare: {}".format(Prius.name, Prius.odometer, Prius.fuel, Prius.current_fare_distance, Prius.get_fare()))
+
+
+
+
+
+
+
+main()
