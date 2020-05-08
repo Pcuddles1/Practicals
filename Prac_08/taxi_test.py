b24ab9ad434fee54a05bf7f07285e73dba75cac0
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-08 17:45:30 +1000

    added more refined text output

--- Prac_08/taxi_test.py
+++ Prac_08/taxi_test.py
@@ -4,14 +4,10 @@ from Prac_08.taxi import Taxi
 def main():
     Prius = Taxi(100, "prius 1")
     Prius.drive(40)
-    print("Name: {}\nOdometer: {}\nFuel: {}\nDistance: {}\nFare: {}".format(Prius.name, Prius.odometer, Prius.fuel,
-                                                                            Prius.current_fare_distance,
-                                                                            Prius.get_fare()))
+    print(Prius,"\n$",Prius.get_fare())
     Prius.start_fare()
     Prius.drive(100)
-    print("\nName: {}\nOdometer: {}\nFuel: {}\nDistance: {}\nFare: {}".format(Prius.name, Prius.odometer, Prius.fuel,
-                                                                              Prius.current_fare_distance,
-                                                                              Prius.get_fare()))
+    print(Prius,"\n$",Prius.get_fare())
 
 
 main()
