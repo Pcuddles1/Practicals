2eb5139e8f0a4611cc2e6298397fc27750dbcb8d
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-08 18:27:11 +1000

    added list

---
+++ Prac_08/taxi_simulator.py
@@ -0,0 +1,5 @@
+from Prac_08.taxi import Taxi
+from Prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
