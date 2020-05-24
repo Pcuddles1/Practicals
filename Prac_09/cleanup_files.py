4e933fad0035b94551468ccf7f74edc5ac022605
Pcuddles <patrick.cudmore@my.jcu.edu.au>
2020-05-24 15:21:33 +1000

    initial commit

---
+++ Prac_09/cleanup_files.py
@@ -0,0 +1,46 @@
+import shutil
+import os
+
+
+def main():
+    print("Starting directory is: {}".format(os.getcwd()))
+    os.chdir('Lyrics\Christmas')
+    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+    try :
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+    for filename in os.listdir('.'):
+        # Ignore directories, just process files
+        if os.path.isdir(filename):
+            continue
+
+        new_name = get_fixed_filename(filename)
+        print("Renaming {} to {}".format(filename, new_name))
+
+def get_fixed_filename(filename):
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+def demo_walk():
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+
+        for filename in filenames:
+            current_name = os.path.join(directory_name, filename)
+            print(current_name)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            print(new_name)
+            try:
+                os.rename(current_name, new_name)
+            except FileExistsError:
+                pass
+
+#main()
+demo_walk()
